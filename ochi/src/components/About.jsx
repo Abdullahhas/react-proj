a10f93afa53907792a80b2cf3e99f0428025e678
abdullahhas <abdullah312003@gmail.com>
2024-12-07 00:51:49 +0500

    start ochi app and completing  about page

--- ochi/src/components/About.jsx
+++ ochi/src/components/About.jsx
@@ -7,12 +7,16 @@ function About() {
         Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell products, explain complex ideas, and hire great people.
     </h1>
 
-    <div className='w-full border-t-[1px] border-[#a1b562] pt-10 mt-20'>
+    <div className='w-full flex gap-5 border-t-[1px] border-[#a1b562] pt-10 mt-20'>
     <div className='w-1/2 '>
         <h1 className='text-5xl'>Our approach:</h1>
         <button className='uppercase flex gap-10 items-center py-6 px-10 bg-zinc-900 mt-10 rounded-full text-white' >Read More
         <div className='w-2 h-2 bg-zinc-100 rounded-full'></div>
         </button>
+    </div>
+
+    <div className='w-1/2 h-[70vh] rounded-3xl bg-[#b0c859]'>
+
     </div>
     </div>
     </div>
